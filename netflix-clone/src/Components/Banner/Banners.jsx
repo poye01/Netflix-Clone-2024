22f2e8a359e86f7dff11e1ead6241488370134a4
poye01 <blenberhanu01@gmail.com>
2024-11-22 10:35:40 +0300

    row

--- netflix-clone/src/Components/Banner/Banners.jsx
+++ netflix-clone/src/Components/Banner/Banners.jsx
@@ -18,7 +18,6 @@ const Banners = () => {
       }
     })();
   }, []);
-
   function truncate(str, n) {
     return str?.length > n ? str.substr(0, n - 1) + "..." : str;
   }
--- netflix-clone/src/pages/Home/Home.jsx
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../../Components/Header/Header";
 import Banners from "../../Components/Banner/Banners";
+import RowList from "../../Components/Rows/RowList/RowList";
 import Footer from "../../Components/Footer/Footer";
 function Home() {
   return (
     <>
       <Header />
       <Banners />
+      <RowList />
       <Footer />
     </>
   );
