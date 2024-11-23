0798d605c977a74768ca3e1db8a8a0a19ad4b5af
poye01 <blenberhanu01@gmail.com>
2024-11-23 14:09:03 +0300

    deploy

--- netflix-clone/vite.config.js
+++ netflix-clone/vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/Netflix-Clone-2024/", 
+  base: "/Netflix-Clone-2024-Clone-2024/", // Base path for deployment
   plugins: [react()],
+  build: {
+    outDir: "dist", // Output directory
+    assetsDir: "assets", // Directory for static assets
+  },
 });
